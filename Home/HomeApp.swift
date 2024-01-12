36fa70375fc42f47afbdd9045eab40d3f1d1ca77
Dawn Liu <dliu@lcs.on.ca>
2024-01-12 08:36:56 -0500

    Initial Commit

---
+++ Home/HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeApp.swift
+//  Home
+//
+//  Created by Chenxi Liu on 2024/1/12.
+//
+
+import SwiftUI
+
+@main
+struct HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
